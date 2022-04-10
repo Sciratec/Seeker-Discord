76b31a49e964a440667fb8f6b4327a19e91f9ba2
Sciratec <84640654+Sciratec@users.noreply.github.com>
2022-04-10 14:31:10 -0400

    Whispers results to user, added formatting to text

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 from os import getenv
 from dotenv import load_dotenv
-from matplotlib.pyplot import isinteractive
 from vt import virustotal
 from ht import hatchingTriage
 
@@ -14,12 +13,16 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'Bot: {client.user} is now live.')
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     msg = message.content
-#     await client.process_commands(message)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content
+
+    if msg.startswith('$'):
+        await message.channel.send("*<Ticker Name>*")
+
+    await client.process_commands(message)
 
 # @client.command(name="vt", description="Search Virustotal and pull IOCS. Example: !vt <SHA256>")
 # async def vt(ctx, sha256Hash):
@@ -55,37 +58,42 @@ async def on_ready():
 @client.command(name="ht", brief="Search Hatching Triage by using a SHA256 hash", description="Search Hatching Triage and pull IOCS/Configs. Example: !ht <SHA256>")
 async def ht(ctx, sha256Hash):
 
+    user = ctx.author
+
     if ctx.channel.name == ("malware-analysis"):
         c2s, cncURLs, iocURLs, iocIPS, signatures = hatchingTriage(sha256Hash)
 
+        await user.send(f"Results for {sha256Hash}:")
+
         if iocURLs and type(iocURLs) == list:
-            await ctx.send("IOC Domain(s):")
+            await user.send("IOC Domain(s):")
             stripped_domains = "\n".join(iocURLs)
-            await ctx.send(stripped_domains)
+            await user.send(f"```{stripped_domains}```")
         if iocIPS and type(iocIPS) == list:
-            await ctx.send("\n\nIOC IP(s):")
+            await user.send("\n\nIOC IP(s):")
             stripped_ips = "\n".join(iocIPS)
-            await ctx.send(stripped_ips)
+            await user.send(f"```{stripped_ips}```")
         if c2s and type(c2s) == list:
-            await ctx.send("\n\nRule/C2:")
+            await user.send("\n\nRule/C2:")
             for a in c2s:
                 stripped_c2s = "/".join(a)
-                await ctx.send(stripped_c2s)
+                await user.send(f"```{stripped_c2s}```")
         if cncURLs and type(cncURLs) == list:
-            await ctx.send("\n\nC2 URL(s):")
+            await user.send("\n\nC2 URL(s):")
             for a in cncURLs:
                 stripped_cnc = "\n".join(a)
-                await ctx.send(stripped_cnc)
+                await user.send(f"```{stripped_cnc}```")
         if signatures and type(signatures) == list:
-            await ctx.send("\nSignatures:")
+            await user.send("\nSignatures:")
             for a in signatures:
                 stripped_sig = ", ".join(a)
-                await ctx.send(stripped_sig)
+                await user.send(f"```{stripped_sig}```")
 
 @ht.error
 async def ht_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Provide SHA256 to search.")
+    if ctx.channel.name == ("malware-analysis"):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Provide SHA256 to search.")
 
 @client.command(name="clear")
 @commands.has_role('Owner')
